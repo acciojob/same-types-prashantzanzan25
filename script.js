dc8568e0f8a17209487b313f99281911e847c811
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-17 15:27:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 function isSameType(value1, value2) {
- // check if both values are NaN using the Number.isNaN function
- if (Number.isNaN(value1) && Number.isNaN(value2)) {
+  // check if both values are NaN
+  if (Number.isNaN(Number(value1)) && Number.isNaN(Number(value2))) {
     return true;
- }
+  }
 
- // check if both values are of the same type using the typeof operator
- return typeof value1 === typeof value2;
+  // check if both values are of the same type after parsing them
+  return typeof (value1 === "string" ? parseFloat(value1) : value1) === typeof (value2 === "string" ? parseFloat(value2) : value2);
 }
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
-cy.on("uncaught:exception", (err) => {
- if (err.message.includes("The following error originated from your application code")) {
-    return false;
- }
-});
